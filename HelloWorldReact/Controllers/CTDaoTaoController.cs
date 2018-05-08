2dac0e231a397bf41ddafee98fa81fedf858be5a
HieuTran <hieutran.mtg@gmail.com>
2018-05-08 22:15:48 +0700

    Hiếu

---
+++ HelloWorldReact/Controllers/CTDaoTaoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorldReact.Controllers
+{
+    public class CTDaoTaoController : Controller
+    {
+        // GET: CTDaoTao
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
